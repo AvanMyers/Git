91ecc13b8970ddd9fd78dddf5c367d777e3b9661
AvanMyers <smail1341@gmail.com>
2022-08-05 15:30:38 +0300

    dz

---
+++ DZ1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            string zodiac;
+            int age;
+            string work;
+
+            Console.Write("Введити ваше имя: ");
+            name = Console.ReadLine();
+            Console.Write("Введите ваш возраст: ");
+            age = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите ваш знак зодиака: ");
+            zodiac = Console.ReadLine();
+            Console.Write("Введи вашу должность: ");
+            work = Console.ReadLine();
+
+            Console.WriteLine($"Здравствуйте {name}, " +
+                $"мы рады вас приветсвовать на должности {work}, ему {age} и " +
+                $"по горосокпу он {zodiac}");
+
+            Console.ReadLine();
+        }
+    }
+}
